a4ac72dd5d90a17ad4418f091dc8e123cb51e367
Patrick Neulichedl <patrickneu@hotmail.de>
2021-06-04 13:39:00 +0200

    Initialize project.

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,28 @@
+package com.example.demo
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/demo")
+class DemoController {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @GetMapping
+    fun get() {
+        logger.info("Hello DemoApp")
+    }
+
+}
